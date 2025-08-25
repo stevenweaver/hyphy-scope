9c571446588e4f92f0abf906340a1988d22df748
Steven Weaver <sweaver@temple.edu>
2025-08-25 14:44:22 -0600

    updating configs

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,14 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: vitePreprocess({
+		// Explicitly ensure no deprecated options
+		typescript: {
+			compilerOptions: {
+				verbatimModuleSyntax: true
+			}
+		}
+	}),
 	kit: {
 		adapter: adapter()
 	}
